1a46b62a3e1f1a2a04b7331256155dfe7e1d3dfb
Arwildo <arwildo@gmail.com>
2024-02-11 21:21:44 +0900

    add landing page hero section

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -3,54 +3,50 @@
 </script>
 
 <nav class="absolute z-10 w-full border-b border-black/5 dark:border-white/5 lg:border-transparent">
-    <div class="relative flex flex-wrap items-center justify-between gap-6 py-3 md:gap-0 md:py-4">
+    <div class="relative flex items-center justify-between gap-6 py-3 md:gap-0 md:py-4">
         <div class="relative z-20 flex w-full justify-between md:px-0 lg:w-max">
-
-            <div class="relative z-20 flex w-full justify-between md:px-0 lg:w-max">
-                <a href="/#home" aria-label="logo" class="flex items-center space-x-2">
-                    <div aria-hidden="true" class="flex space-x-1">
-                        <div class="h-4 w-4"></div>
-                        <img class="h-4 w-4" src={logo} alt="Clean, fast, and modern portfolio website base">
-                    </div>
-                    <span class="text-2xl font-bold text-gray-900 dark:text-white">Portix.</span>
-                </a>
-
-                <div class="relative flex max-h-10 items-center lg:hidden">
-                    <button aria-label="humburger" id="hamburger" class="relative -mr-6 p-6">
-                        <div aria-hidden="true" id="line" class="m-auto h-0.5 w-5 rounded transition duration-300 bg-gray-300"></div>
-                        <div aria-hidden="true" id="line2" class="m-auto mt-2 h-0.5 w-5 rounded transition duration-300 bg-gray-300"></div>
-                    </button>
+            <a href="/#home" aria-label="logo" class="flex items-center space-x-2">
+                <div aria-hidden="true" class="flex space-x-1">
+                    <div class="h-4 w-4"></div>
+                    <img class="h-4 w-4" src={logo} alt="Clean, fast, and modern portfolio website base">
                 </div>
+                <span class="text-2xl font-bold text-gray-900 dark:text-white">Portix.</span>
+            </a>
+
+            <div class="relative flex max-h-10 items-center lg:hidden">
+                <button aria-label="hamburger" id="hamburger" class="relative -mr-6 p-6">
+                    <div aria-hidden="true" id="line" class="m-auto h-0.5 w-5 rounded transition duration-300 bg-gray-300"></div>
+                    <div aria-hidden="true" id="line2" class="m-auto mt-2 h-0.5 w-5 rounded transition duration-300 bg-gray-300"></div>
+                </button>
             </div>
-            
-            <div aria-hidden="true" class="fixed inset-0 z-10 h-screen w-screen origin-bottom scale-y-0 backdrop-blur-2xl transition duration-500 bg-gray-900/70 lg:hidden"></div>
-            <div class="invisible absolute top-full left-0 z-20 w-full origin-top-right translate-y-1 scale-90 flex-col flex-wrap justify-end gap-6 rounded-3xl border border-gray-100 bg-white p-8 opacity-0 shadow-2xl shadow-gray-600/10 transition-all duration-300 border-gray-700 dark:bg-gray-800 lg:visible lg:relative lg:flex lg:w-7/12 lg:translate-y-0 lg:scale-100 lg:flex-row lg:items-center lg:gap-0 lg:border-none lg:bg-transparent lg:p-0 lg:opacity-100 lg:shadow-none">
-                <div class="w-full text-gray-600 dark:text-gray-200 lg:w-auto lg:pr-4 lg:pt-0">
-                    <ul class="flex flex-col lg:flex-row gap-6 tracking-wide lg:gap-0 lg:text-sm">
-                        <li>
-                            <a href="/projects" class="hover:text-primary block transition dark:hover:text-white md:px-4">
-                                <span>Projects</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="/about" class="hover:text-primary block transition dark:hover:text-white md:px-4">
-                                <span>About</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="/contact" class="hover:text-primary block transition dark:hover:text-white md:px-4">
-                                <span>Contact</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="/updates" class="flex gap-2 font-semibold text-gray-700 transition hover:text-primary dark:text-white dark:hover:text-white md:px-4">
-                                <span>Updates</span>
-                                <span class="flex rounded-full bg-primary/20 px-1.5 py-0.5 text-xs tracking-wider text-purple-700 dark:bg-white/10 dark:text-orange-300"> new</span>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
+        </div>
 
+        <div aria-hidden="true" class="fixed inset-0 z-10 h-screen w-screen origin-bottom scale-y-0 backdrop-blur-2xl transition duration-500 bg-gray-900/70 lg:hidden"></div>
+        <div class="invisible absolute top-full right-0 z-20 w-full origin-top-right translate-y-1 scale-90 flex-col flex-wrap justify-end gap-6 rounded-3xl border border-gray-100 p-8 opacity-0 transition-all duration-300 bg-gray-800 lg:visible lg:relative lg:flex lg:w-7/12 lg:translate-y-0 lg:scale-100 lg:flex-row lg:items-center lg:gap-0 lg:border-none lg:bg-transparent lg:p-0 lg:opacity-100 lg:shadow-none">
+            <div class="w-full text-gray-600 dark:text-gray-200 lg:w-auto lg:pr-4 lg:pt-0">
+                <ul class="flex flex-col lg:flex-row gap-6 tracking-wide lg:gap-0 lg:text-sm justify-end"> <!-- Changed justify-end here -->
+                    <li>
+                        <a href="/projects" class="hover:text-primary block transition dark:hover:text-white md:px-4">
+                            <span>Projects</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/about" class="hover:text-primary block transition dark:hover:text-white md:px-4">
+                            <span>About</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/contact" class="hover:text-primary block transition dark:hover:text-white md:px-4">
+                            <span>Contact</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/updates" class="flex gap-2 font-semibold text-gray-700 transition hover:text-primary dark:text-white dark:hover:text-white md:px-4">
+                            <span>Updates</span>
+                            <span class="flex rounded-full bg-primary/20 px-1.5 py-0.5 text-xs tracking-wider text-purple-700 dark:bg-white/10 dark:text-orange-300"> new</span>
+                        </a>
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,69 @@
+<script>
+    import JavascriptLogo from '../assets/images/javascript.svg'
+</script>
+
+<div class="relative" id="home">
+    <div aria-hidden="true" class="absolute inset-0 grid grid-cols-2 -space-x-52 opacity-20">
+        <div class="blur-[106px] h-56 bg-gradient-to-br from-primary to-purple-400 dark:from-blue-700"></div>
+        <div class="blur-[106px] h-32 bg-gradient-to-r from-cyan-400 to-sky-300 dark:to-indigo-600"></div>
+    </div>
+    <div class="relative pt-36 ml-auto">
+        <div class="lg:w-2/3 text-center mx-auto">
+            <h1 class="text-white font-bold text-5xl md:text-6xl xl:text-7xl">Clean, Fast, Modern <br><span class="text-white">Web App.</span></h1>
+            <p class="mt-8 text-gray-300">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio incidunt nam itaque sed eius modi error totam sit illum. Voluptas doloribus asperiores quaerat aperiam. Quidem harum omnis beatae ipsum soluta!</p>
+            <div class="mt-16 flex flex-wrap justify-center gap-y-4 gap-x-6">
+                <a
+                    href="/contact"
+                    class="relative flex h-11 w-full items-center justify-center px-6 before:absolute before:inset-0 before:rounded-full before:bg-primary before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 sm:w-max"
+                >
+                    <span class="relative text-base font-semibold text-white"
+                    >Let's Chat</span
+                    >
+                </a>
+                <a
+                    href="/more"
+                    class="relative flex h-11 w-full items-center justify-center px-6 before:absolute before:inset-0 before:rounded-full before:border before:border-transparent before:bg-primary/10 before:bg-gradient-to-b before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 dark:before:border-gray-700 dark:before:bg-gray-800 sm:w-max"
+                >
+                    <span
+                    class="relative text-base font-semibold text-white"
+                    >Up Work</span
+                    >
+                </a>
+            </div>
+            <div class="hidden py-8 mt-16 border-y border-gray-800 sm:flex justify-between">
+                <div class="text-left">
+                    <h6 class="text-lg font-semibold text-white">3D Website</h6>
+                    <p class="mt-2 text-gray-500">Some text here</p>
+                </div>
+                <div class="text-left">
+                    <h6 class="text-lg font-semibold text-white">Desktop Software</h6>
+                    <p class="mt-2 text-gray-500">Some text here</p>
+                </div>
+                <div class="text-left">
+                    <h6 class="text-lg font-semibold text-white">Script</h6>
+                    <p class="mt-2 text-gray-500">Some text here</p>
+                </div>
+            </div>
+        </div>
+        <div class="mt-12 grid grid-cols-3 sm:grid-cols-4 md:grid-cols-6">
+            <div class="p-4 grayscale transition duration-200 hover:grayscale-0">
+                <img src={JavascriptLogo} class="h-7 w-auto mx-auto" loading="lazy" alt="Javascript"/>
+            </div>
+            <div class="p-4 grayscale transition duration-200 hover:grayscale-0">
+                <img src={JavascriptLogo} class="h-7 w-auto mx-auto" loading="lazy" alt="Javascript"/>
+            </div>
+            <div class="p-4 grayscale transition duration-200 hover:grayscale-0">
+                <img src={JavascriptLogo} class="h-7 w-auto mx-auto" loading="lazy" alt="Javascript"/>
+            </div>
+            <div class="p-4 grayscale transition duration-200 hover:grayscale-0">
+                <img src={JavascriptLogo} class="h-7 w-auto mx-auto" loading="lazy" alt="Javascript"/>
+            </div>
+            <div class="p-4 grayscale transition duration-200 hover:grayscale-0">
+                <img src={JavascriptLogo} class="h-7 w-auto mx-auto" loading="lazy" alt="Javascript"/>
+            </div>
+            <div class="p-4 grayscale transition duration-200 hover:grayscale-0">
+                <img src={JavascriptLogo} class="h-7 w-auto mx-auto" loading="lazy" alt="Javascript"/>
+            </div>
+        </div>
+    </div>
+</div>
